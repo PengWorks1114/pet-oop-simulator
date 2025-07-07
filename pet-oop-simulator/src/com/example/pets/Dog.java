86561d9248a5e0e0bb5a24852856d043a5a1d82c
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-07 14:15:09 +0900

    新增狗類別

--- pet-oop-simulator/src/com/example/pets/Dog.java
+++ pet-oop-simulator/src/com/example/pets/Dog.java
@@ -1,4 +1,18 @@
 package com.example.pets;
 
-public class Dog {
+public class Dog extends Pet implements Talkable {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + "狗叫");
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("我是狗,我叫: " + getName());
+    }
 }
