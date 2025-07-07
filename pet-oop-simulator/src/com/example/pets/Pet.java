ee4aa2b2ab1e605189f44782a17988a7dfbfec9a
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-07 14:32:02 +0900

    修改Pet類別

--- pet-oop-simulator/src/com/example/pets/Pet.java
+++ pet-oop-simulator/src/com/example/pets/Pet.java
@@ -1,17 +1,35 @@
 package com.example.pets;
 
-//共通的抽象父類別,不能直接被new
 public abstract class Pet {
     private String name;
+    protected int hunger; // 0（飽）～100（非常餓）
 
     public Pet(String name) {
         this.name = name;
+        this.hunger = 50; // 預設中等飢餓
     }
 
     public String getName() {
         return name;
     }
 
-    //強制所有仔類別都要定義自我介紹的方式
+    public int getHunger() {
+        return hunger;
+    }
+
+    public void feed() {
+        if (hunger > 0) {
+            hunger -= 20;
+            if (hunger < 0) hunger = 0;
+            System.out.println(name + " 吃飽了！目前飢餓值：" + hunger);
+        } else {
+            System.out.println(name + " 已經吃得很飽了！");
+        }
+    }
+
+    public void showStatus() {
+        System.out.println(name + " 的飢餓值是 " + hunger + "（0=飽，100=餓）");
+    }
+
     public abstract void introduce();
 }
