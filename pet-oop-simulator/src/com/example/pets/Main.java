0ad2dd56958ce7dfae18e9d87e1628a9ca54220d
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-07 14:18:39 +0900

    修改Main類別內容

--- pet-oop-simulator/src/com/example/pets/Main.java
+++ pet-oop-simulator/src/com/example/pets/Main.java
@@ -1,10 +1,23 @@
 package com.example.pets;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Talkable[] pets = {
+                new Dog("小黑"),
+                new Cat("小花"),
+                new Parrot("小綠")
+        };
 
+        for (Talkable pet : pets) {
+            // 多型：介面統一呼叫不同類別的 speak 方法
+            pet.speak();
 
+            // 若物件同時也是 Pet，則呼叫 introduce 方法
+            if (pet instanceof Pet) {
+                ((Pet) pet).introduce();
+            }
+
+            System.out.println("-----------");
+        }
     }
 }
