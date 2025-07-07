9de2e0c61cb0f82d810219187fcef14bb625fd4f
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-07 14:12:18 +0900

    建立共通的抽象父類別Pet

--- pet-oop-simulator/src/com/example/pets/Pet.java
+++ pet-oop-simulator/src/com/example/pets/Pet.java
@@ -1,4 +1,17 @@
 package com.example.pets;
 
-public class Pet {
+//共通的抽象父類別,不能直接被new
+public abstract class Pet {
+    private String name;
+
+    public Pet(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //強制所有仔類別都要定義自我介紹的方式
+    public abstract void introduce();
 }
